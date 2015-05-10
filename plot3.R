30337763e2011d56e96213b6c3cab3e2b35a9b7c
Jesús Salceda <jesus.salceda@gmail.com>
2015-05-10 18:50:29 +0200

    Correct wrong legend
    Legends are including now the name with underscores (Sub_metering_1)

--- plot3.R
+++ plot3.R
@@ -41,8 +41,8 @@ lines(powerdata$DateTime, powerdata$Sub_metering_2, type = "l", col = "red")
 lines(powerdata$DateTime, powerdata$Sub_metering_3, type = "l", col = "blue")
 
 ## Added legends to plot
-legend("topright",c("Sub Metering 1","Sub Metering 2", "Sub Metering 3"),
-       lty=c(1,1,1),lwd=c(2.5,2.5,2.5),
+legend("topright",c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"),
+       lty=c(1,1,1),lwd=c(1,1,1),
        col=c("black","blue","red"))
 
 ## Close graphics Device
